e5738190033b2a6ae0f7a492e6bb1aa3f061479f
edrickalema <alemaedrick85@gmail.com>
2025-10-22 22:10:08 +0300

    feat: add Notification and Comment models to post controller

--- backend/src/controllers/post.contoller.js
+++ backend/src/controllers/post.contoller.js
@@ -3,7 +3,8 @@ import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { getAuth } from "@clerk/express";
 import cloudinary from "../config/cloudinary.js";
-
+import Notification from "../models/notification.model.js";
+import Comment from "../models/comment.model.js";
 // get posts
 export const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
