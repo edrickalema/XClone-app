1b893aaaffa9f299c251787dab0d8dc632f2c512
edrickalema <alemaedrick85@gmail.com>
2025-10-23 18:55:56 +0300

    Auth implemented

--- frontend/app/(auth)/index.tsx
+++ frontend/app/(auth)/index.tsx
@@ -1,6 +1,15 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useSocialAuth } from "@/hooks/useSocialAuth";
+import { useState } from "react";
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const { isLoading, handleSocialAuth } = useSocialAuth();
   return (
     <View className='flex-1 bg-white'>
       <View className='flex-1 px-8 justify-between'>
@@ -8,17 +17,17 @@ export default function Index() {
           {/* APP DEMO IMAGE */}
           <View className='items-center'>
             <Image
-              source={require("../../assets/images/auth1.png")}
+              source={require("../../assets/images/auth2.png")}
               className='size-96'
               resizeMode='contain'
             />
           </View>
 
-          <View className='flex-col gap-2'>
+          <View className='flex-col gap-3 mt-4'>
             <TouchableOpacity
-              onPress={() => {}}
+              onPress={() => handleSocialAuth("oauth_google")}
               className='flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6'
-              disabled={true}
+              disabled={isLoading}
               style={{
                 shadowColor: "#000",
                 shadowOffset: { width: 0, height: 1 },
@@ -27,23 +36,28 @@ export default function Index() {
                 elevation: 2,
               }}
             >
-              <View>
-                <Image
-                  resizeMode='contain'
-                  className='size-10 mr-3'
-                  source={require("../../assets/images/google.png")}
-                />
-              </View>
-
-              <Text className='text-black font-bold text-base'>
-                Continue with google
-              </Text>
+              {isLoading ? (
+                <ActivityIndicator size={"small"} color={"#6875e"} />
+              ) : (
+                <View className=' flex-row items-center justify-center'>
+                  <View>
+                    <Image
+                      resizeMode='contain'
+                      className='size-10 mr-3'
+                      source={require("../../assets/images/google.png")}
+                    />
+                  </View>
+                  <Text className='text-black font-bold text-base'>
+                    Continue with Google
+                  </Text>
+                </View>
+              )}
             </TouchableOpacity>
 
             <TouchableOpacity
-              onPress={() => {}}
+              onPress={() => handleSocialAuth("oauth_apple")}
               className='flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6'
-              disabled={true}
+              disabled={isLoading}
               style={{
                 shadowColor: "#000",
                 shadowOffset: { width: 0, height: 1 },
@@ -52,19 +66,31 @@ export default function Index() {
                 elevation: 2,
               }}
             >
-              <View>
-                <Image
-                  resizeMode='contain'
-                  className='size-8 mr-3'
-                  source={require("../../assets/images/apple.png")}
-                />
-              </View>
-
-              <Text className='text-black font-bold text-base'>
-                Continue with Apple
-              </Text>
+              {isLoading ? (
+                <ActivityIndicator size={"small"} color={"#6875e"} />
+              ) : (
+                <View className=" flex-row items-center justify-center'">
+                  <View>
+                    <Image
+                      resizeMode='contain'
+                      className='size-8 mr-3'
+                      source={require("../../assets/images/apple.png")}
+                    />
+                  </View>
+                  <Text className='text-black font-bold text-base'>
+                    Continue with Apple
+                  </Text>
+                </View>
+              )}
             </TouchableOpacity>
           </View>
+
+          <Text className='text-center max-w-sm text-gray-500 text-xs ledaing-4 mt-6 px-2'>
+            By signing up, you agree to our{" "}
+            <Text className='text-blue-500'>Terms</Text> {", "}
+            <Text className='text-blue-500'>Privacy policy</Text> {", and"}{" "}
+            <Text className='text-blue-500'>Cookie use</Text>.
+          </Text>
         </View>
       </View>
     </View>
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { SignedOut, useClerk } from "@clerk/clerk-expo";
+import {
+  Button,
+  Text,
+  View,
+  Pressable,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const { signOut } = useClerk();
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-4xl font-bold text-blue-500'>Welcome to Alema!</Text>
+      <Text className='text-4xl font-bold text-blue-500'>
+        Welcome to Alema!
+      </Text>
+
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text>Out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
